7dfd2bcd09b7220ef97b97e2e1d90acb8f3d4d54
psmits1984 <psmits1984@users.noreply.github.com>
2026-04-20 08:46:47 +0000

    deploy: 35f3664c82fdf3d1708afe9833bab136a37222d0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "47933491"
+    serviceWorkerVersion: "1460700091"
   }
 });
